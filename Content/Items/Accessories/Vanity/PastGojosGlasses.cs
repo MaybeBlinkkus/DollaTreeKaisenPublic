c74bd8a8fcf8499277b423747c0d1dc0ee948c4b
blinkkusofficial <168491516+blinkkusofficial@users.noreply.github.com>
2024-09-02 01:36:02 -0400

    Import file batch again

---
+++ Content/Items/Accessories/Vanity/PastGojosGlasses.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NeviliscoVanity.Content.Items.Accessories.Vanity
+{
+    [AutoloadEquip(EquipType.Front)]
+    public class PastGojosGlasses : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Content.Items.Accessories.Vanity";
+        public override void SetDefaults()
+        {
+            Item.width = Item.height = 24;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = 0;
+            Item.vanity = true;
+            Item.accessory = true;
+        }
+
+        public override void AddRecipes() => CreateRecipe().AddTile(TileID.Loom).Register();
+    }
+}
