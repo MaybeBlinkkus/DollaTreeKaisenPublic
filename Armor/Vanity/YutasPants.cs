cde5ecdc4586cde7f798aa1ce6200b7f732abe56
blinkkusofficial <168491516+blinkkusofficial@users.noreply.github.com>
2024-09-02 01:28:09 -0400

    Initial upload batch 2

---
+++ Armor/Vanity/YutasPants.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NeviliscoVanity.Content.Items.Armor.Vanity
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class YutasPants : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Content.Items.Armor.Vanity";
+        public override void SetDefaults()
+        {
+            Item.width = 22;
+            Item.height = 18;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = 0;
+            Item.vanity = true;
+        }
+
+        public override void AddRecipes() => CreateRecipe().AddTile(TileID.Loom).Register();
+    }
+}
